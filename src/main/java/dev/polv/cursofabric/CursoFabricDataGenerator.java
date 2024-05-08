03a77dfb121099bc17c0336e568de02b92710314
Pol Vallverdu <86187892+polvallverdu@users.noreply.github.com>
2024-05-08 11:04:07 +0200

    Initial Commit

---
+++ src/main/java/dev/polv/cursofabric/CursoFabricDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.polv.cursofabric;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CursoFabricDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
